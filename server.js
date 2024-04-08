53b62487480f7290fd8f21172c64690e68d4aa2a
pranavaselva <pranava.m@kalvium.community>
2024-04-08 12:31:48 +0530

    mongodb connected

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const app = express()
+const {connectiondata,modeldata}=require("./mongodata")
+
+function gettingData(){
+    return modeldata.db.readyState === 1
+}
 
 
 app.get('/ping', (req, res) => {
-    res.send('Hello Node App')
+    const calling  = gettingData()
+    let  connectionStatus = calling ? "database connected" : "connection failed"
+    res.send(connectionStatus)
 })
 
 
 app.listen(3000, () => {
+    connectiondata()
     console.log('Node API app is running on port 3000')
 })
