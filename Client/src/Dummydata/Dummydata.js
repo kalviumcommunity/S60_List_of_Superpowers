31c935fc83408f3f712e8566c6ec8a55fff3a66d
pranavaselva <pranava.m@kalvium.community>
2024-04-09 10:34:29 +0530

    displayed dummy data

---
+++ Client/src/Dummydata/Dummydata.js
@@ -0,0 +1,9 @@
+const dummyData = {
+    Super_Power_Category : "Super-Human-level",
+    Name : "Black Panther (T'Challa)",
+    Super_Power : "Enhanced strength, speed, agility, and senses due to the heart-shaped herb.",
+    Image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5eV0qtlXQ9xVY9aWYUBXRwYP61up1ycyctmluYyESuA&s",
+}
+
+
+export default dummyData
